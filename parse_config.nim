10780ff77ac3f3d7a0c82d5f4f3eefab3cc41bc2
flywind <43030857+xflywind@users.noreply.github.com>
2019-09-16 22:04:31 +0800

    define some func to process xml

--- parse_config.nim
+++ parse_config.nim
@@ -1,11 +1,83 @@
 import os, streams
-import json #, xmlParser, yaml
+import json #, parseXml, yaml
 import types
 
 
 proc parseConfigXml(filename: string)
 proc parseConfigJson(filename: string): Analysis
 proc parseConfigYaml(filename: string)
+
+
+
+proc parseAction(xd: var XmlParser) = 
+  # name
+  xd.next
+  if xd.attrkey == "name":
+    echo xd.attrValue
+    # description
+    xd.next
+  if xd.attrkey == "description":
+    echo xd.attrValue
+  # >
+    xd.next
+  # query
+    xd.next
+  # data
+  if xd.elementName == "query":
+    xd.next
+    echo xd.charData
+  # query end
+    xd.next
+  # action end
+  xd.next
+  # next action
+  xd.next
+
+proc parseSchema(xd: var XmlParser): Schema = 
+  # data
+  xd.next
+  result = xd.charData
+  # schema end
+  xd.next
+  # next schema
+  xd.next
+  
+proc parseTable(xd: var XmlParser): Table = 
+  var 
+    count: string
+    name: string
+    description: string
+    query: string
+
+    
+  xd.next
+  if xd.attrKey == "count":
+    count = xd.attrValue
+    xd.next
+  if xd.attrKey == "name":
+    name = xd.attrValue
+    xd.next
+  if xd.attrKey == "description":
+    description = xd.attrValue
+    xd.next
+  if xd.kind == xmlElementStart and xd.elementName == "query":
+    xd.next
+    query = xd.charData
+    # query end
+    xd.next
+    # table end
+    xd.next
+  xd.next
+  return initTable(count, name, description, query)
+
+proc parseIndex(xd: var XmlParser): Index = 
+  # data
+  xd.next
+  result = xd.charData
+  # index end
+  xd.next
+  # next index
+  xd.next
 
 
 proc parseConfigureFile(filename: string): Analysis = 
