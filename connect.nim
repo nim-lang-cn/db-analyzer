309ae81f5de8a8950f3e02dbbceac73de0c4f40d
flywind <xzsflywind@gmail.com>
2019-10-07 22:38:44 +0800

    update

--- connect.nim
+++ connect.nim
@@ -1,3 +1,19 @@
-import db_postgres
-let db = open("localhost", "user", "password", "dbname")
-db.close()
+# import db_postgres
+import streams
+import strutils
+
+var strm = newFileStream("./sqls/account.sql", fmRead)
+var line = ""
+var lines = ""
+if not isNil(strm):
+  while strm.readLine(line):
+    lines &= line
+    if line.endsWith(";"):
+      echo lines
+      lines = ""
+  strm.close()
+
+
+
+# let db = open("localhost", "user", "password", "dbname")
+# db.close()
